aacd40ef46bf6c46f9cf53589004a0bfdabd10e9
Anthony <anthony.hseb@hotmail.com>
2017-05-08 15:56:43 +0200

    removed pypy and py26 form tox environments because tests were failing on travis-ci althought they were succeeding locally (URL requests being blocked on travis)

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ],
     test_suite='tests',
