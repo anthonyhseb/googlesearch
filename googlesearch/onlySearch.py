39f7d74a98812198b6eaa86cc8921e5ff16860b6
Rakesh S R <rakeshsagalagatte@gmail.com>
2020-09-04 08:16:39 +0530

    Fixed for Python 3

--- googlesearch/onlySearch.py
+++
@@ -1,6 +0,0 @@
-from googlesearch import GoogleSearch
-
-res = GoogleSearch().search("rakeshsagalagatte")
-for result in res.results:
-    print("Title: " + result.title)
-    print("Content: " + result.getText())
